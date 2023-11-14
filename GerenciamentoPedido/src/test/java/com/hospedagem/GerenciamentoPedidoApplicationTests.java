16b25e8712111b2dd971632113e1d9f8e1ecb1cf
IsabellaInocente <132359890+IsabellaInocente@users.noreply.github.com>
2023-11-14 10:25:47 -0300

    Add files via upload

---
+++ GerenciamentoPedido/src/test/java/com/hospedagem/GerenciamentoPedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospedagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoPedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
